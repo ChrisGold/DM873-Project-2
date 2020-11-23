347c21dbf8eb415b8ee33c43a7becaff69dadae7
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-11-23 14:17:11 +0100

    Fix warnings

--- layers/Dense.py
+++ layers/Dense.py
@@ -1,22 +1,20 @@
 import tensorflow as tf
 from tensorflow.keras.layers import *
 
+
 class Dense(Layer):
     def __init__(self, units=32, **kwargs):
         super(Dense, self).__init__(**kwargs)
         self.units = units
         bias_init = tf.zeros_initializer()
         self.bias = tf.Variable(initial_value=bias_init(shape=(self.units,), dtype='float32'), trainable=True)
+        self.w = None # w is initialized in build
 
     def build(self, input_shape):
         weights_init = tf.random_normal_initializer()(shape=(input_shape[-1], self.units), dtype='float32')
-
         self.w = self.add_weight(initial_value=weights_init, trainable=True)
-
         super(Dense, self).build(input_shape)
 
-    def call(self, input, **kwargs):
-        placeholder = tf.matmul(input, self.weights) + self.bias
-        return tf.relu(placeholder)
-
-
+    def call(self, x, **kwargs):
+        y = tf.matmul(x, self.weights) + self.bias
+        return tf.nn.relu(y)
