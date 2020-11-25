c020f46097159bccea949e811c082483b9496357
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-11-25 10:44:10 +0100

    Fix Pylint issues

--- layers/Conv2D.py
+++ layers/Conv2D.py
@@ -1,13 +1,19 @@
+from typing import Any, Union
+
 import tensorflow as tf
 from keras import backend as K
 from tensorflow.keras.layers import *
 
 # pylint: disable=g-classes-have-attributes
+from tensorflow.python.distribute.sharded_variable import ShardedVariable
+from tensorflow.python.ops.variables import PartitionedVariable
 
 
 class Conv2D(Layer):
+    bias: Union[Union[PartitionedVariable, ShardedVariable, Conv2D], Any]
+    kernel: Union[Union[PartitionedVariable, ShardedVariable, Conv2D], Any]
+
     def __init__(self, filters=32, strides=1, **kwargs):
-        # TODO: Compute output shape
         super(Conv2D, self).__init__(**kwargs)
         self.filters = filters
         self.bias_init = tf.zeros_initializer()
@@ -17,12 +23,11 @@ class Conv2D(Layer):
         self.padding = 'valid'
 
     def build(self, input_shape):
-
         super(Conv2D, self).build(input_shape)
-        self.bias = self.add_weight(initial_value=self.bias_init(shape=(self.filters,), dtype='float32'), trainable=True)
+        self.bias = self.add_weight(initial_value=self.bias_init(shape=(self.filters,), dtype='float32'),
+                                    trainable=True)
         self.kernel = self.add_weight(initial_value=self.kernel_init, trainable=True)
 
     def call(self, x, **kwargs):
         feature_maps = K.conv2d(x, self.kernel, self.strides, self.padding)
         return tf.nn.relu(feature_maps)
-
