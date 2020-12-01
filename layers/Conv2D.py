29324d3234e2f0c2c036f093c585c23851e5f125
camillamariebach <56353611+camillamariebach@users.noreply.github.com>
2020-12-01 12:11:02 +0100

    work on compute_output_shape()

--- layers/Conv2D.py
+++ layers/Conv2D.py
@@ -1,47 +1,51 @@
 from typing import Any, Union
 
 import tensorflow as tf
-from keras import backend as K, activations
+from keras import activations
 from keras.utils import conv_utils
 from tensorflow.keras.layers import *
-
 # pylint: disable=g-classes-have-attributes
 from tensorflow.python.distribute.sharded_variable import ShardedVariable
 from tensorflow.python.ops.variables import PartitionedVariable
 
 
 class Conv2D(Layer):
-    # TODO: Compute output shape
     bias: Union[Union[PartitionedVariable, ShardedVariable, Conv2D], Any]
     kernel: Union[Union[PartitionedVariable, ShardedVariable, Conv2D], Any]
 
-    def __init__(self, filters=32, strides=1, padding='valid', activation='relu', **kwargs):
+    def __init__(self, filters=32, strides=(1, 1), padding='valid', activation='relu', dilation_rate=(1, 1), **kwargs):
         super(Conv2D, self).__init__(**kwargs)
         self.filters = filters
-        self.bias_init = tf.zeros_initializer()
         self.bias = None
-        self.kernel_size = (2, 2, self.filters)
+        self.kernel_size = (2, 2)
         self.kernel_init = tf.keras.initializers.GlorotUniform()(self.kernel_size)
         self.kernel = None
         self.strides = strides
         self.padding = padding
         self.activation = activation
+        self.dilation_rate = dilation_rate
 
     def build(self, input_shape):
         super(Conv2D, self).build(input_shape)
-        self.bias = self.add_weight(initial_value=self.bias_init(shape=(self.filters,), dtype='float32'),
+        self.bias = self.add_weight(name='bias',
+                                    shape=(self.filters,),
+                                    dtype='float32',
+                                    initializer=tf.zeros_initializer(),
                                     trainable=True)
-        self.kernel = self.add_weight(initial_value=self.kernel_init, trainable=True)
+        self.kernel = self.add_weight(shape=self.kernel_size,
+                                      initializer=tf.keras.initializers.GlorotUniform(),
+                                      trainable=True)
 
     def call(self, x, **kwargs):
-        y = K.conv2d(x, self.kernel, self.strides, self.padding)
+        y = tf.keras.backend.conv2d(x, self.kernel, strides=(1, 1), padding='valid', data_format=None,
+                                    dilation_rate=(1, 1))
         activation = activations.get(self.activation)
         if activation is not None:
             y = activation(y)
         return y
 
     def compute_output_shape(self, input_shape):
-        batchsize = input_shape[0]
+        batch_size = input_shape[0]
         convX = conv_utils.conv_output_length(
             input_shape[1],
             self.kernel_size[0],
@@ -56,4 +60,4 @@ class Conv2D(Layer):
             stride=self.strides[1],
             dilation=self.dilation_rate[1]
         )
-        return batchsize, convX, convY, self.filters
+        return batch_size, convX, convY, self.filters
