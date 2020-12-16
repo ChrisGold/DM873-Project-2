1966fc58e149a0470839b01f559c2a71df4aa663
Christian Goldapp <c.goldapp@tu-braunschweig.de>
2020-12-16 17:44:42 +0100

    Test MaxPooling.py

--- layers/MaxPooling.py
+++ layers/MaxPooling.py
@@ -4,7 +4,7 @@ from tensorflow.keras.layers import *
 
 
 class MaxPooling(Layer):
-    def __init__(self, pool_size=(2, 2), strides=None, padding='valid', **kwargs):
+    def __init__(self, pool_size=(2, 2), strides=None, padding='VALID', **kwargs):
         super(MaxPooling, self).__init__(**kwargs)
         self.pool_size = pool_size
         self.strides = strides
@@ -20,3 +20,33 @@ class MaxPooling(Layer):
     def compute_output_shape(self, input_shape):
         conv_len = conv_utils.conv_output_length(input_shape[1], self.pool_size[0], self.padding, self.strides[0])
         return input_shape[0], conv_len, input_shape[2]
+
+if __name__ == '__main__':
+
+    def create_model():
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Input(shape = (224, 224, 3)),
+            Dense(units=32),
+            Dense(units=32),
+            Dense(units=64),
+            MaxPooling(pool_size=(2,2)),
+            Dense(units=128),
+            ])
+        return model
+
+    def create_keras_model():
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Input(shape = (224, 224, 3)),
+            tf.keras.layers.Dense(units=32),
+            tf.keras.layers.Dense(units=32),
+            tf.keras.layers.Dense(units=64),
+            tf.keras.layers.MaxPooling2D(),
+            tf.keras.layers.Dense(units=128),
+            ])
+        return model
+
+
+    model = create_model()
+    print(model.summary())
+    keras = create_keras_model()
+    print(keras.summary())
